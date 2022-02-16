2527fc87a104ac88bddd3526ed79962c29484817
Slobodan Stanojević <bobanstanoja@hotmail.com>
2022-02-16 16:11:27 +0100

    -Added BaseValidatorFactory to public-api.ts -Added license
    (cherry picked from commit 5f7e517c851bc6eeed64758c209585a7a461ce42)

--- valdr-ng/projects/valdr-ng/src/public-api.ts
+++ valdr-ng/projects/valdr-ng/src/public-api.ts
@@ -3,5 +3,6 @@
  */
 
 export * from './lib/valdr-ng.service';
+export {BaseValidatorFactory} from './lib/validators/base-validator-factory';
 export {ValdrNgModule} from './lib/valdr-ng.module';
 export * from './lib/model';
